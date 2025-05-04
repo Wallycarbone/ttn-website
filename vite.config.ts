3dc16f9d19fde4b0661193645cb46542592522cc
Wallycarbone <wallycarbone@gmail.com>
2025-05-04 15:31:43 -0400

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './client/src'),
-      '@assets': path.resolve(__dirname, './client/src/assets'),
-      '@components': path.resolve(__dirname, './client/src/components'),
-      '@hooks': path.resolve(__dirname, './client/src/hooks'),
-      '@lib': path.resolve(__dirname, './client/src/lib'),
-      '@pages': path.resolve(__dirname, './client/src/pages'),
-      '@shared': path.resolve(__dirname, './shared'),
-    },
-  },
-  build: {
-    outDir: 'dist/client',
-    emptyOutDir: true,
-    sourcemap: true,
-  },
-  server: {
-    proxy: {
-      '/api': 'http://localhost:3000',
-      '/ws': {
-        target: 'ws://localhost:3000',
-        ws: true,
-      },
-    },
-  },
-});
+})
